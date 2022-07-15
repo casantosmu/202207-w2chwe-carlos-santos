f0385ca4a133a520b7f2bb414a59d241497bb933
Carlos <casantosmu@gmail.com>
2022-07-15 22:30:51 +0200

    Exclude lines between class members except after single line

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,11 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+  },
 };
