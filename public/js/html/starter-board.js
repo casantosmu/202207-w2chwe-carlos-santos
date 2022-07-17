590f4d0c27d8e397f32c96d5f381e59f9cb82ce7
Carlos <casantosmu@gmail.com>
2022-07-17 02:45:57 +0200

    Added print starter grid function

---
+++ public/js/html/starter-board.js
@@ -0,0 +1,26 @@
+const getCellTemplateHTML = (id) =>
+  `<div class="board__cell" data-id="${id}" tabindex="${id}"></div>`;
+
+const getRowTemplateHTML = (innerHTML) =>
+  `<div class="board__row">${innerHTML}</div>`;
+
+const getGridInnerElements = (rows, columns) => {
+  let loopCounter = 0;
+  let gridHTML = "";
+  for (let rowIndex = 0; rowIndex < rows; rowIndex += 1) {
+    let generatedCellsHTML = "";
+    for (let columnsIndex = 0; columnsIndex < columns; columnsIndex += 1) {
+      generatedCellsHTML += getCellTemplateHTML(loopCounter);
+      loopCounter += 1;
+    }
+    gridHTML += getRowTemplateHTML(generatedCellsHTML);
+  }
+  return gridHTML;
+};
+
+export const printStarterGrid = (rows, columns) => {
+  const gridElement = document.querySelector(".js-grid");
+  gridElement.innerHTML = getGridInnerElements(rows, columns);
+};
+
+export default printStarterGrid;
