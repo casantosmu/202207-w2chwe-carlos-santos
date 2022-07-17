b0e9a9b332d94b7357e4ddf07cb1f2d8585af392
Carlos <casantosmu@gmail.com>
2022-07-17 03:55:29 +0200

    Modified board HTML to 64x64 && commented unnecesary code

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,38 @@
 import { printStarterGrid } from "./html/starter-board.js";
-import {
-  areAllDead,
-  getCellsBoard,
-  getNextCellsStatus,
-} from "./cells/cells-board.js";
+// import {
+//   areAllDead,
+//   getCellsBoard,
+//   getNextCellsStatus,
+// } from "./cells/cells-board.js";
 
-let currentCellStatus = [
-  false,
-  false,
-  false,
-  true,
-  true,
-  true,
-  false,
-  false,
-  true,
-];
+// const currentCellStatus = [
+//   false,
+//   false,
+//   false,
+//   true,
+//   true,
+//   true,
+//   false,
+//   false,
+//   true,
+// ];
 
-const cellsBoardRows = 3;
-const cellsBoardColumns = currentCellStatus.length / cellsBoardRows;
+// const cellsBoardRows = 3;
+// const cellsBoardColumns = currentCellStatus.length / cellsBoardRows;
 
-printStarterGrid(cellsBoardRows, cellsBoardColumns);
+printStarterGrid(32, 32);
 
-const timedLoop = setInterval(() => {
-  if (areAllDead(currentCellStatus)) {
-    clearInterval(timedLoop);
-  }
+// const timedLoop = setInterval(() => {
+//   if (areAllDead(currentCellStatus)) {
+//     clearInterval(timedLoop);
+//   }
 
-  const currentCellsBoard = getCellsBoard(
-    cellsBoardRows,
-    cellsBoardColumns,
-    currentCellStatus
-  );
+//   const currentCellsBoard = getCellsBoard(
+//     cellsBoardRows,
+//     cellsBoardColumns,
+//     currentCellStatus
+//   );
 
-  console.log(currentCellsBoard);
-  currentCellStatus = getNextCellsStatus(currentCellsBoard);
-}, 500);
+//   console.log(currentCellsBoard);
+//   currentCellStatus = getNextCellsStatus(currentCellsBoard);
+// }, 500);
